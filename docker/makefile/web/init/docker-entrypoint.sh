26ec324516070605cdf2871f61093d5eea866907
zhanhui_h <zhanhui1987@163.com>
2019-12-17 00:02:37 +0800

    优化创建docker镜像的README文件；给web容器增加初始化mysql中，blog表的功能，并增加创建默认superuser账号的功能

--- docker/makefile/web/init/docker-entrypoint.sh
+++ docker/makefile/web/init/docker-entrypoint.sh
@@ -18,5 +18,16 @@ then
     service cron restart;
 fi
 
+# init mysql table for django and create default superuser
+django_manage_file="/root/docker-django/z1987_web/manage.py"
+if [ -f ${django_manage_file} ];
+then
+    /usr/bin/python ${django_manage_file} makemigrations app_blog
+    /usr/bin/python ${django_manage_file} migrate
+
+    # create superuesr for django
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | /usr/bin/python ${django_manage_file} shell
+fi
+
 # create a process that cannot be ended
 tail -f /dev/null
